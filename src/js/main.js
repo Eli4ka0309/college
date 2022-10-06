99d6e39a3172db035aa3d1aef6b588b90db0aeb3
Eli4ka0309 <100150012+Eli4ka0309@users.noreply.github.com>
2022-10-06 15:54:23 +0300

    add new changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,2 @@
-$(document).ready(function () {
-    $('.header_nav a').click(function () {
-        $('.header_nav a').removeClass('active')
-        $(this).toggleClass('active')
-    });
-
-    //horizontal_scroll//
-
-    $('.horizontal_scroll_inner').slick({
-        infinite: true,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear'
-    });
-
-
-
-});
-
+let kek = "hello";
+console.log(kek);
